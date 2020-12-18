fda5b3932fc97a837e580a82a4c78c4bfdce9eb6
felipeasynvest <felipe.barbosa@easynvest.com.br>
2020-12-17 22:43:32 -0300

    init: deploy step

---
+++ src/br/com/dynamic/deploy_dev/Deploy.groovy
@@ -0,0 +1,36 @@
+package br.com.dynamic.deploy_dev
+
+class Deploy{
+    def call (jenkins, jobParams) {
+
+        jenkins.podTemplate(
+            containers: [
+                jenkins.containerTemplate(name: 'helm', image: 'alpine/helm:3.4.1', ttyEnabled: true, command: 'cat', alwaysPullImage: false)
+            ],
+            yamlMergeStrategy: jenkins.merge(),
+            workspaceVolume: jenkins.persistentVolumeClaimWorkspaceVolume(
+                claimName: "pvc-${jenkins.env.JENKINS_AGENT_NAME}",
+                readOnly: false
+            )
+        )
+        {
+            jenkins.node(jenkins.POD_LABEL){
+                jenkins.container('helm'){
+                    jenkins.echo "Deploy Step"
+
+                    jenkins.withKubeConfig([
+                        credentialsId: '',
+                        serverUrl: 'http://127.0.0.1',
+                    ]) {
+                        jenkins.sh label: 'Deploy on minikube 🚀', script:"""
+                            helm package \${HELM_CHART_NAME} &&
+                            helm upgrade --install --atomic --debug --namespace=\${KUBE_NAMESPACE} \${HELM_RELEASE_NAME} --set-string image.tag=\${APP_VERSION}.\${GIT_COMMIT} ./\${HELM_CHART_NAME}*.tgz
+                        """
+                    }
+
+                    jenkins.echo 'Deploy successful ! 🌟'
+                }
+            }
+        }
+    }
+}
