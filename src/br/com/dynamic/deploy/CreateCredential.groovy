92dfb4dc0b3e3c12d5163d44e76e0ac811ef8e95
Felipe Barbosa <felipebarbosa@santander.com.br>
2021-01-07 12:19:10 -0300

    build: pass credential ID and description as parameters

--- src/br/com/dynamic/deploy/CreateCredential.groovy
+++ src/br/com/dynamic/deploy/CreateCredential.groovy
@@ -6,23 +6,25 @@ import com.cloudbees.plugins.credentials.domains.*
 import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl
 import hudson.util.Secret
 
+class CreateCredential{
 // CREATE A CREDENTIAL WITH GIVEN SERVICEACCOUNT TOKEN 
-@NonCPS
-static def createCredential(String serviceAccountToken) {
-    String keyfile = "/tmp/key"
-    Credentials c = (Credentials) new StringCredentialsImpl(
-              CredentialsScope.GLOBAL,
-              "minikube-user",
-              "User for pipeline demo",
-              Secret.fromString(serviceAccountToken)
-            )
-    def ksm1 = new CertificateCredentialsImpl.UploadedKeyStoreSource(keyfile)
-    Credentials ck1 = new CertificateCredentialsImpl(CredentialsScope.GLOBAL,
-              "minikube-user",
-              "User for pipeline demo",
-              serviceAccountToken
-              , ksm1)
-    
-    SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), c)
-    SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), ck1)
+  @NonCPS
+  def createCredential(String serviceAccountToken, String credentialId, String credentialDescription) {
+      String keyfile = "/tmp/key"
+      Credentials c = (Credentials) new StringCredentialsImpl(
+                CredentialsScope.GLOBAL,
+                credentialId,
+                credentialDescription,
+                Secret.fromString(serviceAccountToken)
+              )
+      def ksm1 = new CertificateCredentialsImpl.UploadedKeyStoreSource(keyfile)
+      Credentials ck1 = new CertificateCredentialsImpl(CredentialsScope.GLOBAL,
+                credentialId,
+                credentialDescription,
+                serviceAccountToken
+                , ksm1)
+      
+      SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), c)
+      SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), ck1)
+  }
 }
--- src/br/com/dynamic/deploy/Deploy.groovy
+++ src/br/com/dynamic/deploy/Deploy.groovy
@@ -3,37 +3,40 @@ package br.com.dynamic.deploy
 import br.com.dynamic.deploy.CreateCredential
 
 class Deploy{
+    String credentialId = "minikube-user"
+    String credentialDescription = "User for pipeline demo"
+
     def call (jenkins) {
 
         // DEFAULT POD SERVICEACCOUNT 
         jenkins.env.SERVICEACCOUNTTOKEN="/var/run/secrets/kubernetes.io/serviceaccount/token"
         // DEFAULT POD TOKEN 
         jenkins.env.TOKEN= jenkins.sh script:"cat ${jenkins.env.SERVICEACCOUNTTOKEN}", returnStdout: true
-        CreateCredential.createCredential(jenkins.env.TOKEN)
+        CreateCredential c = new CreateCredential()
+        c.createCredential(jenkins.env.TOKEN, credentialId, credentialDescription)
 
-        jenkins.withKubeConfig([
-                        credentialsId: "minikube-user",
+        jenkins.podTemplate(
+            containers: [
+                jenkins.containerTemplate(name: 'helm', image: 'alpine/helm:3.4.1', ttyEnabled: true, command: 'cat', alwaysPullImage: false)
+            ],
+            yamlMergeStrategy: jenkins.merge(),
+            workspaceVolume: jenkins.persistentVolumeClaimWorkspaceVolume(
+                claimName: "pvc-${jenkins.env.JENKINS_AGENT_NAME}",
+                readOnly: false
+            )
+        )
+        {
+            jenkins.node(jenkins.POD_LABEL){
+                jenkins.container('helm'){
+                    jenkins.echo "Deploy Step"
+                    jenkins.withKubeConfig([
+                        credentialsId: credentialId,
                         serverUrl: 'https://kubernetes.default.svc/api',
                     ]) {
-
-            jenkins.podTemplate(
-                containers: [
-                    jenkins.containerTemplate(name: 'helm', image: 'alpine/helm:3.4.1', ttyEnabled: true, command: 'cat', alwaysPullImage: false)
-                ],
-                yamlMergeStrategy: jenkins.merge(),
-                workspaceVolume: jenkins.persistentVolumeClaimWorkspaceVolume(
-                    claimName: "pvc-${jenkins.env.JENKINS_AGENT_NAME}",
-                    readOnly: false
-                )
-            )
-            {
-                jenkins.node(jenkins.POD_LABEL){
-                    jenkins.container('helm'){
-                        jenkins.echo "Deploy Step"
                         jenkins.sh label: 'Deploy on minikube 🚀', script:"""
-                                helm package \${HELM_CHART_NAME} &&
-                                helm upgrade --install --debug --namespace=\${KUBE_NAMESPACE} \${HELM_RELEASE_NAME} --set-string image.tag=\${APP_VERSION}.\${GIT_COMMIT} ./\${HELM_CHART_NAME}*.tgz
-                            """
+                            helm package \${HELM_CHART_NAME} &&
+                            helm upgrade --install --debug --namespace=\${KUBE_NAMESPACE} \${HELM_RELEASE_NAME} --set-string image.tag=\${APP_VERSION}.\${GIT_COMMIT} ./\${HELM_CHART_NAME}*.tgz
+                        """
                     }
 
                     jenkins.echo 'Deploy successful ! 🌟'
