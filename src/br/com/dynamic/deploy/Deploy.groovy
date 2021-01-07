95c5906ae97ae53fdaf166cc2bd09ea469e103e0
Felipe Barbosa <felipebarbosa@santander.com.br>
2021-01-07 14:13:37 -0300

    build: rollback create credential to principal deploy class

--- src/br/com/dynamic/deploy/Deploy.groovy
+++ src/br/com/dynamic/deploy/Deploy.groovy
@@ -1,5 +1,11 @@
 package br.com.dynamic.deploy
 
+import com.cloudbees.plugins.credentials.impl.*
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.domains.*
+import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl
+import hudson.util.Secret
+
 import br.com.dynamic.deploy.CreateCredential
 
 class Deploy{
@@ -12,8 +18,8 @@ class Deploy{
         jenkins.env.SERVICEACCOUNTTOKEN="/var/run/secrets/kubernetes.io/serviceaccount/token"
         // DEFAULT POD TOKEN 
         jenkins.env.TOKEN= jenkins.sh script:"cat ${jenkins.env.SERVICEACCOUNTTOKEN}", returnStdout: true
-        CreateCredential c = new CreateCredential()
-        c.createCredential(jenkins.env.TOKEN, credentialId, credentialDescription)
+        
+        createCredential(jenkins.env.TOKEN, credentialId, credentialDescription)
 
         jenkins.podTemplate(
             containers: [
@@ -43,5 +49,26 @@ class Deploy{
                 }
             }
         }
+    }
+
+    // CREATE A CREDENTIAL WITH GIVEN SERVICEACCOUNT TOKEN 
+    @NonCPS
+    def createCredential(String serviceAccountToken, String credentialId, String credentialDescription) {
+        String keyfile = "/tmp/key"
+        Credentials c = (Credentials) new StringCredentialsImpl(
+                    CredentialsScope.GLOBAL,
+                    credentialId,
+                    credentialDescription,
+                    Secret.fromString(serviceAccountToken)
+                )
+        def ksm1 = new CertificateCredentialsImpl.UploadedKeyStoreSource(keyfile)
+        Credentials ck1 = new CertificateCredentialsImpl(CredentialsScope.GLOBAL,
+                    credentialId,
+                    credentialDescription,
+                    serviceAccountToken
+                    , ksm1)
+        
+        SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), c)
+        SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), ck1)
     }
 }
