9b13db0a59960daf359f110d64e32a92c64a8555
Felipe Barbosa <felipebarbosa@santander.com.br>
2021-01-07 04:28:14 -0300

    build: kube config definied in parent pod

--- src/br/com/dynamic/deploy/Deploy.groovy
+++ src/br/com/dynamic/deploy/Deploy.groovy
@@ -11,28 +11,29 @@ class Deploy{
         jenkins.env.TOKEN= jenkins.sh script:"cat ${jenkins.env.SERVICEACCOUNTTOKEN}", returnStdout: true
         CreateCredential.createCredential(jenkins.env.TOKEN)
 
-        jenkins.podTemplate(
-            containers: [
-                jenkins.containerTemplate(name: 'helm', image: 'alpine/helm:3.4.1', ttyEnabled: true, command: 'cat', alwaysPullImage: false)
-            ],
-            yamlMergeStrategy: jenkins.merge(),
-            workspaceVolume: jenkins.persistentVolumeClaimWorkspaceVolume(
-                claimName: "pvc-${jenkins.env.JENKINS_AGENT_NAME}",
-                readOnly: false
-            )
-        )
-        {
-            jenkins.node(jenkins.POD_LABEL){
-                jenkins.container('helm'){
-                    jenkins.echo "Deploy Step"
-                    jenkins.withKubeConfig([
+        jenkins.withKubeConfig([
                         credentialsId: "minikube-user",
                         serverUrl: 'https://kubernetes.default.svc/api',
                     ]) {
+
+            jenkins.podTemplate(
+                containers: [
+                    jenkins.containerTemplate(name: 'helm', image: 'alpine/helm:3.4.1', ttyEnabled: true, command: 'cat', alwaysPullImage: false)
+                ],
+                yamlMergeStrategy: jenkins.merge(),
+                workspaceVolume: jenkins.persistentVolumeClaimWorkspaceVolume(
+                    claimName: "pvc-${jenkins.env.JENKINS_AGENT_NAME}",
+                    readOnly: false
+                )
+            )
+            {
+                jenkins.node(jenkins.POD_LABEL){
+                    jenkins.container('helm'){
+                        jenkins.echo "Deploy Step"
                         jenkins.sh label: 'Deploy on minikube 🚀', script:"""
-                            helm package \${HELM_CHART_NAME} &&
-                            helm upgrade --install --debug --namespace=\${KUBE_NAMESPACE} \${HELM_RELEASE_NAME} --set-string image.tag=\${APP_VERSION}.\${GIT_COMMIT} ./\${HELM_CHART_NAME}*.tgz
-                        """
+                                helm package \${HELM_CHART_NAME} &&
+                                helm upgrade --install --debug --namespace=\${KUBE_NAMESPACE} \${HELM_RELEASE_NAME} --set-string image.tag=\${APP_VERSION}.\${GIT_COMMIT} ./\${HELM_CHART_NAME}*.tgz
+                            """
                     }
 
                     jenkins.echo 'Deploy successful ! 🌟'
