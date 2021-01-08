2a904dd9e141c67bb462ee96d5c4c328f3f41759
Felipe Barbosa <felipebarbosa@santander.com.br>
2021-01-07 23:47:09 -0300

    build: add class scope

--- src/br/com/dynamic/deploy/CreateCredential.groovy
+++ src/br/com/dynamic/deploy/CreateCredential.groovy
@@ -6,20 +6,22 @@ import com.cloudbees.plugins.credentials.domains.*
 import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl
 import hudson.util.Secret
 
-static def createSecretText(String credentialToken, String credentialId, String credentialDescription){
-  String keyfile = "/tmp/key"
-  Credentials c = (Credentials) new StringCredentialsImpl(
-      CredentialsScope.GLOBAL,
-      credentialId, 
-      credentialDescription, 
-      Secret.fromString(credentialToken))
-  def ksm1 = new CertificateCredentialsImpl.UploadedKeyStoreSource(keyfile)
-  Credentials ck1 = new CertificateCredentialsImpl(
-      CredentialsScope.GLOBAL,
-      credentialId, 
-      credentialDescription, 
-      credentialToken
-      , ksm1)
-  SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), c)
-  SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), ck1)
+class CreateCredential{
+  static def createSecretText(String credentialToken, String credentialId, String credentialDescription){
+    String keyfile = "/tmp/key"
+    Credentials c = (Credentials) new StringCredentialsImpl(
+        CredentialsScope.GLOBAL,
+        credentialId, 
+        credentialDescription, 
+        Secret.fromString(credentialToken))
+    def ksm1 = new CertificateCredentialsImpl.UploadedKeyStoreSource(keyfile)
+    Credentials ck1 = new CertificateCredentialsImpl(
+        CredentialsScope.GLOBAL,
+        credentialId, 
+        credentialDescription, 
+        credentialToken
+        , ksm1)
+    SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), c)
+    SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), ck1)
+  }
 }
